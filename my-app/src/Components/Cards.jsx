305e509b6ba4a6c95a0905df32da49bdb9e41ae0
Umra-Badar20 <umrabadar555@gmail.com>
2026-03-12 14:51:03 +0500

    vite+ bootstrap

---
+++ my-app/src/Components/Cards.jsx
@@ -0,0 +1,45 @@
+import Flower from "../assets/flower.jpg"
+export default function Cards() {
+    return (
+        <div className="container">
+            <div className="row row-cols-1 row-cols-md-3 g-4 mt-3">
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsXWgGDYHAb1g8FDwx_KfAzo01FBKxHF0CBg&s" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a short card.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
