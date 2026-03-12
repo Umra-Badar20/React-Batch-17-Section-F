c3ed7bce794455ec28b64bcafcbf2d302a4e948b
Umra-Badar20 <umrabadar555@gmail.com>
2026-03-12 15:03:38 +0500

    vite+ bootstrap

--- my-app/src/Components/Cards.jsx
+++ my-app/src/Components/Cards.jsx
@@ -1,5 +1,6 @@
-import Flower from "../assets/flower.jpg"
+import FlowerImg from "../assets/flower.jpg"
 export default function Cards() {
+    let flower2 = "Lily"
     return (
         <div className="container">
             <div className="row row-cols-1 row-cols-md-3 g-4 mt-3">
@@ -14,9 +15,9 @@ export default function Cards() {
                 </div>
                 <div className="col">
                     <div className="card h-100">
-                        <img src="..." className="card-img-top" alt="..."/>
+                        <img src={FlowerImg} className="card-img-top" alt="..."/>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
+                                <h5 className="card-title">{flower2}</h5>
                                 <p className="card-text">This is a short card.</p>
                             </div>
                     </div>
